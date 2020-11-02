7eda4e7a3e5d4fac2762df6066e407482a3a667a
ulukayasaban <32439693+ulukayasaban@users.noreply.github.com>
2020-11-02 16:54:16 +0300

    html5 yenidenemesi

---
+++ saban.js
@@ -0,0 +1,31 @@
+window.addEventListener("DOMContentLoaded", function () {
+ 
+        video = document.getElementById("video"),
+        videoObj = { "video": true },
+        errBack = function (error) {
+            console.log("Video oynatmada hata! hata kodu: ", error.code);
+        };
+    
+ 
+    // Kamera görüntüsünün video ya source olarak veriyoruz
+    if (navigator.getUserMedia) { // Standard
+        navigator.getUserMedia(videoObj, function (stream) {
+            video.src = stream;
+            video.play();
+        }, errBack);
+    } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
+        navigator.webkitGetUserMedia(videoObj, function (stream) {
+            video.src = window.webkitURL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    }
+}, false);
+
+
+
+else if(navigator.mozGetUserMedia) { // Firefox-prefixed
+        navigator.mozGetUserMedia(videoObj, function(stream){
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    }
